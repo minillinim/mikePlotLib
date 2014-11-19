26faf67a69428788e391d4f774a6850f9d8ab49d
minillinim <mike@mikeimelfort.com>
2014-11-19 22:48:30 +1000

    version crud

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='mikePlotLib',
-    version='0.0.1',
+    version='1.0.0',
     author='Michael Imelfort',
     author_email='mike@mikeimelfort.com',
     packages=['mikeplotlib'],
